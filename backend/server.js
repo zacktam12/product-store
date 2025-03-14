9dd917e82e9dabb6a76b07c2326e8874683a18a7
hp <zekariastamiru12@gmail.com>
2025-03-14 21:57:32 +0300

    modify configuration

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,6 @@
 /* eslint-env node */
 import express from "express";
-import dotenv from "dotenv";
-import path from "path";
-
-import { connectDB } from "./config/db.js";
-
-import productRoutes from "./routes/product.route.js";
-
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-
-app.use(express.json()); // allows us to accept JSON data in the req.body
-
-app.use("/api/products", productRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.get("*", (_req, res) => {
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-},
-
-app.listen(PORT, () => {
-  connectDB();
-  console.log("Server started at http://localhost:" + PORT);
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
 });
