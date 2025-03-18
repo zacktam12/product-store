e73b81cd45432d248dacfebed8da2646d9daa7c6
hp <zekariastamiru12@gmail.com>
2025-03-18 21:54:17 +0300

    refactor: change import statement for express to CommonJS syntax

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-import express from "express";
+const express = require("express");
 const app = express();
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
