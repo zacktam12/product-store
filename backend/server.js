da381b0dcf2eb5ed792ccb00e30cd0f067ef2be7
hp <zekariastamiru12@gmail.com>
2025-03-21 23:36:19 +0300

    chore: add initial .npmignore

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-/* eslint-env node */
-const express = require("express");
+import express from "express";
 const app = express();
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+
+app.get("/", (req, res) => {
+  res.send("Server is ready 123");
 });
+
+app.listen(5000, () => {
+  console.log("Server started at http://Localhost:5000 heres");
+});
+// 9vtbfx9aqklqxiAp
