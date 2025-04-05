2e5f5a904578d2a3f17957978f56cc0f9a63cd31
hp <zacktam12@users.noreply.github.com>
2025-04-05 23:33:29 +0300

    chore: add initial files and configurations for various modules

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Wrap your app with ChakraProvider
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
     <App />
-  </StrictMode>,
-)
+  </ChakraProvider>
+);
