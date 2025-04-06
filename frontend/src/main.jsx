4e6d2f67b6f3edb1902f7f9c3dd7938ca0d0ef5e
hp <zacktam12@users.noreply.github.com>
2025-04-06 22:54:10 +0300

    feat: implement Navbar component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App";
 
 // Wrap your app with ChakraProvider
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
     <App />
   </ChakraProvider>
 );
